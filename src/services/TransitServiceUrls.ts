ba9fac2ba530613f5ec89c65538e20dbef84e4fd
Kaylee Edwards <kaylee.jean.edwards@gmail.com>
2021-11-07 17:58:53 -0600

    Fix mixed-content issues

--- src/services/TransitServiceUrls.ts
+++ src/services/TransitServiceUrls.ts
@@ -3,7 +3,7 @@ const CORS_PROXY = '';
 //const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
 
 export const TransitServiceUrls = {
-  getFetchRoutesUrl: () => `${CORS_PROXY}http://svc.metrotransit.org/nextripv2/routes`,
-  getFetchDirectionsUrl: (routeId: string) => `${CORS_PROXY}http://svc.metrotransit.org/nextripv2/directions/${routeId}`,
-  getFetchStopsUrl: (routeId: string, directionId: string) => `${CORS_PROXY}http://svc.metrotransit.org/nextripv2/stops/${routeId}/${directionId}`
+  getFetchRoutesUrl: () => `${CORS_PROXY}https://svc.metrotransit.org/nextripv2/routes`,
+  getFetchDirectionsUrl: (routeId: string) => `${CORS_PROXY}https://svc.metrotransit.org/nextripv2/directions/${routeId}`,
+  getFetchStopsUrl: (routeId: string, directionId: string) => `${CORS_PROXY}https://svc.metrotransit.org/nextripv2/stops/${routeId}/${directionId}`
 };
